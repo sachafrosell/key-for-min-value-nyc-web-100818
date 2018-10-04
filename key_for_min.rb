2ce5080da1b8c74db13e1fe5bfcc9e713ae7a7a3
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 19:43:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+
 
 def key_for_min_value(name_hash)
-
+  int = 1000
+  name_hash.each do |name, value|
+    if value < int
+      int = value
+    end 
+  end
+  name_hash.key(int)
 end
+
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+key_for_min_value(veggies)
